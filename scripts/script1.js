0fa771e2338c95fba1c7a7851e525b5a95929e4a
mukhtiarmemon <mukhtiar001@yahoo.com>
2023-06-14 10:26:01 +0500

    first commit 14-6

---
+++ scripts/script1.js
@@ -0,0 +1,33 @@
+// let username = 'Mukhtiar';
+// console.log(username);
+// username = 5;
+// console.log(typeof(username));
+
+//objects
+/* let employee = {
+    empName: 'Mukhtiar',
+    emplAge: 42
+};
+employee.empName = 'John';
+console.log(employee.empName); */
+
+//arrays
+
+// let selectedColors = ['red', 'blue'];
+// console.log(selectedColors[0]);
+
+//functions
+
+function greet() {
+    console.log('welcome');
+}
+
+function greet2(name) {
+    console.log('welcome' +' '+name);
+}
+
+function square(number) {
+    return number*number;
+}
+
+console.log('Square: ' +square(4));
